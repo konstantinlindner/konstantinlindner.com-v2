901cbc59936c0534c782337f01dac8e123d8aee1
Konstantin Lindner <hello@konte.se>
2023-06-22 14:43:54 -0700

    remove graphql

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,14 +3,11 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../styles/theme';
 import Fonts from '../styles/fonts';
-import { ApolloProvider } from '@apollo/client';
 import { client } from '../apollo';
 import { ReactNode } from 'react';
 
 const Providers = ({ children }: { readonly children: ReactNode }) => (
-  <ApolloProvider client={client}>
-    <ChakraProvider theme={theme}>{children}</ChakraProvider>
-  </ApolloProvider>
+  <ChakraProvider theme={theme}>{children}</ChakraProvider>
 );
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
